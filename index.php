4105ec57cee66e98565787d33932a6635d43d21c
Josh Staff <josh.staff@openplay.co.uk>
2018-12-12 12:24:09 +0000

    Deliberately bad CS

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+class Index {
+
+    function someDeliberatelyBadCodeStyle()    {
+
+    }
+
+
+    protected function more() {
+        return [
+          'heloo', 'world','okay', 'nomore'
+        ];
+    }
+
+
+
+    private function oneMore($test   = null) {
+        return function(...  $args)
+        {
+           return array_walk($this->more(),function( $param ){
+               if( !$param ) {
+                   $this->tellAJudge();
+               }
+           });
+        };
+    }
+
+    public function tellAJudge() {
+        return'ok!';
+    }
+
+}
