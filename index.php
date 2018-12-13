c2391c4b4b75d5cbaa04fbf763c6656192c9590d
Josh Staff <josh.staff@openplay.co.uk>
2018-12-13 16:40:42 +0000

    WIP

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ class Index
 
     const test = 'here';
 
-    public function noVisibility()   {     }
+    public function noVisibility()   {      }
 
     private function oneMore($test = null)   {
         return function (...  $args) {
